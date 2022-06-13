ecd656b9eb205b188a2838550e0a108af9296193
Alex Nygren <alex.nygren@kinaknowledge.com>
2022-06-13 13:29:56 -0400

    Removed the success and warn functios from core.

--- core.lisp
+++ core.lisp
@@ -1792,21 +1792,7 @@
     
     })
 
-  (defglobal warn
-      (defclog { `prefix: "⚠️  "  `background: "yellow" `foreground: "red" })
-      {
-      `description: "Prefixes a warning symbol prior to the arguments to the console.  Otherwise the same as console.log."
-      `usage:["args0:*" "argsN:*" ]
-      `tags: ["log" "warning" "error" "signal" "output" "notify" "defclog"]
-      })
-  
-  (defglobal success
-      (defclog { `color: `green `prefix: "✓  " })
-      {
-      `description: "Prefixes a green checkmark symbol prior to the arguments to the console.  Otherwise the same as console.log."
-      `usage:["args0:*" "argsN:*" ]
-      `tags: ["log" "warning" "notify" "signal" "output" "ok" "success" "defclog"]
-      })
+ 
 
   
     true
