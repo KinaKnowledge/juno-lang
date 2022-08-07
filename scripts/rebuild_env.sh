6eb8b9b2a40efc86e92cd2e2db8befc579461673
Alex Nygren <alex.nygren@kinaknowledge.com>
2022-08-07 12:40:09 -0400

    Initial commit

---
+++ scripts/rebuild_env.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+cat <<EOI | lib/juno
+(import "src/base-io.lisp")
+(import "src/build-tools.lisp")
+(rebuild_env)
+EOI
+
+cat <<EOI | lib/juno
+(import "src/base-io.lisp")
+(import "src/build-tools.lisp")
+(import "working/save_env.lisp")
+EOI
+
+echo "verifying juno.js"
+cat <<EOI | lib/juno js/juno
+  *env_config*
+EOI
+
+# cat a.j | lib/juno ;cat b.j | lib/juno ;lib/juno js/juno.js
