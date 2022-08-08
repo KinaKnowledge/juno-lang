cf8172a8accfe16e3e990ef6f17234b5dea13f42
Alex Nygren <alex.nygren@kinaknowledge.com>
2022-08-08 07:06:34 -0400

    refined quoted package carryover to browser environment.

--- js/browser_boot.js
+++ js/browser_boot.js
@@ -1,9 +1,16 @@
 
 addEventListener('DOMContentLoaded', async function() {
   try {
-    await env.set_global('html_lib',html_lib)
-    await env.set_global('browser_workspace',browser_workspace);
-    await env.evaluate('(eval html_lib)');
+    //await env.set_global('html_lib',html_lib)
+    //await env.set_global('browser_workspace',browser_workspace);
+    console.log("Starting browser boot..")
+    console.log(await env.evaluate("(sort (core/symbols))"))
+    let pkg=await env.evaluate("core/html_package");
+    console.log("pkg: ",pkg);
+    await env.evaluate(pkg);
+    //await env.evaluate('(console.log html_lib)')
+    await env.evaluate('(html_lib)');
+    await env.evaluate('(eval browser_repl_package)');
     await env.evaluate('(eval browser_workspace)');
     globalThis.env = env;
   } catch (ex) {
