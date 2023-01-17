9baccc27d2ff1358f7ef9064d810e50e0b8f4155
Alex Nygren <alex.nygren@kinaknowledge.com>
2023-01-17 12:48:00 -0500

    Improved build script and error detection. Builds a compiled binary as an additional step.

--- scripts/rebuild_env.sh
+++ scripts/rebuild_env.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [ -f "./BUILD_DATE.txt" ]; then 
+   echo "Removing Build time stamp."
+   rm "./BUILD_DATE.txt"
+fi
+   
 cat <<EOI | lib/juno
 (import "src/base-io.lisp")
 (import "src/build-tools.lisp")
@@ -15,13 +20,51 @@ cat <<EOI | lib/juno
 EOI
 
 cat <<EOI | lib/juno
-(import "src/base-io.lisp")
-(import "src/build-tools.lisp")
-(import "working/save_env.lisp")
+(try 
+  (progn
+    (declare (function save_environment)
+             (global save_environment))
+    (import "src/base-io.lisp")
+    (import "src/build-tools.lisp")
+    (import "src/core-ext.lisp")
+    (import "pkg/server_env.juno")
+    (import "pkg/lz-string.juno")
+    (import "pkg/sys.juno")
+    (import "pkg/save_env.juno")
+    (save_environment { compile: true emit_as: "bin/build.tmp" } )
+    (exit 0))
+  (catch Error (e)
+     (progn
+       (console.error e)
+       (exit 1))))
 EOI
+
+if [ $? -ne 0 ];then 
+  echo "Build failed"
+  exit 1
+fi
 
 echo "verifying juno.js"
 cat <<EOI | lib/juno js/juno.js
   *env_config*
+  (write_text_file "./BUILD_DATE.txt" (prop *env_config* "build"))
 EOI
+
+SYSTEM=`uname -s|tr '[:upper:]' '[:lower:]'`
+MACHINE=`uname -m|tr '[:upper:]' '[:lower:]'`
+
+BINNAME="bin/juno.$MACHINE.$SYSTEM"
+
+
+if [ -f "./BUILD_DATE.txt" ]; then    
+    # lib/juno --compile -A bin/build.tmp js/juno.js
+    if [ -f "bin/build.tmp" ]; then
+	mv "bin/build.tmp" $BINNAME
+	echo
+	echo "Build Completed - $BINNAME constructed"
+    fi
+else
+    echo "Build failed"
+fi
+   
 
