d972d695f2a97340ec1f92f680eb65895606ff6c
Alex Nygren <alex.nygren@kinaknowledge.com>
2023-04-26 09:01:33 -0400

    uuid generation function added to core (uuid)

--- src/core.lisp
+++ src/core.lisp
@@ -4903,6 +4903,16 @@
      `tags: ["dependencies" "tree" "required" "dependency"]
    })
  
+(defun_sync uuid ()
+  (javascript "return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+     (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16));")
+  {
+      description: "Returns a UUID"
+      usage: []
+      tags: ["uuid" "id" "generate"]
+  })
+
+
 (defun_sync pad_left (value pad_amount padchar)
    (-> (+ "" value) `padStart pad_amount padchar)
    {
